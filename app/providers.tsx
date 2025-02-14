c473a9bb7bdaa7cd67aae09fa1f1d7b936c68dd2
yuanyuexiang <yuanyuexiang@126.com>
2025-02-14 16:04:43 +0800

    update code

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { getDefaultWallets, RainbowKitProvider, connectorsForWallets } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider,createConfig } from "wagmi";
@@ -76,8 +76,8 @@ const queryClient = new QueryClient();
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-                <RainbowKitProvider locale="zh-CN">
+            <QueryClientProvider client={queryClient} >
+                <RainbowKitProvider locale="zh-CN" initialChain={mantleSepoliaTestnet}>
                     {children}
                 </RainbowKitProvider>
             </QueryClientProvider>
